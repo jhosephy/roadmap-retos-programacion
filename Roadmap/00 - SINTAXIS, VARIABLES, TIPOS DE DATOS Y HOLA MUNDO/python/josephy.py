6e57400c2222a34ae7e9944e0ba0bc13d71134cf
jhosephy <pglobal55@gmail.com>
2024-07-22 00:53:04 -0500

    Create josephy.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/josephy.py
@@ -0,0 +1,36 @@
+# https://www.python.org
+
+# Comentario en una línea
+
+"""
+Esto también es
+un comentario en
+varias líneas
+"""
+
+'''
+Esto también es
+un comentario en
+varias líneas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+# Phyton no tiene constantes, todos son variables
+MY_CONSTANT = "Mi constante" # Por convención
+
+# En Phyton existen 4 tipos de datos primitivos
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("¡Hola, Phyton!")
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+
